5eb236656c952c59bb40ff4d527eb87f98a48fa3
henry22 <henry811010@gmail.com>
2021-04-06 16:17:10 +0800

    add Link to MiniPalette

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 	};
 	return (
 		<Switch>
-			<Route exact path='/' render={() => <PaletteList palettes={seedColors} />} />
+			<Route exact path='/' render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} />
 			<Route
 				exact
 				path='/palette/:id'
