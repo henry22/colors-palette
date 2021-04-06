2e2c90160f6af22f6c6245e465026687bfd34e1d
henry22 <henry811010@gmail.com>
2021-04-06 19:04:48 +0800

    displaying shades in SingleColorPalette

--- src/App.js
+++ src/App.js
@@ -11,13 +11,22 @@ function App() {
 	};
 	return (
 		<Switch>
+			<Route
+				exact
+				path='/palette/:paletteId/:colorId'
+				render={(routeProps) => (
+					<SingleColorPalette
+						palette={generatePalette(findPalette(routeProps.match.params.paletteId))}
+						colorId={routeProps.match.params.colorId}
+					/>
+				)}
+			/>
 			<Route exact path='/' render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} />
 			<Route
 				exact
 				path='/palette/:id'
 				render={(routeProps) => <Palette palette={generatePalette(findPalette(routeProps.match.params.id))} />}
 			/>
-			<Route exact path='/palette/:paletteId/:colorId' render={() => <SingleColorPalette />} />
 		</Switch>
 	);
 }
