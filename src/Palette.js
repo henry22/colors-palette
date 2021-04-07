33ff86a30a5b6b4e88281d06fcafcd3dade9c992
henry22 <henry811010@gmail.com>
2021-04-07 15:01:54 +0800

    move styles into new folder

--- src/Palette.js
+++ src/Palette.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
+import styles from './styles/PaletteStyles';
 import { withStyles } from '@material-ui/styles';
-import './Palette.css';
 import Navbar from './Navbar';
 import PaletteFooter from './PaletteFooter';
-
-const styles = {
-	Palette: {
-		height: '100vh',
-		display: 'flex',
-		flexDirection: 'column'
-	},
-	colors: {
-		height: '90%'
-	}
-};
 
 class Palette extends Component {
 	constructor(props) {
