8d286e2691e8c4939968601820d8b28edad41055
henry22 <henry811010@gmail.com>
2021-04-13 20:16:10 +0800

    animate palette delete

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import MiniPalette from './MiniPalette';
 import styles from './styles/PaletteListStyles';
 import { withStyles } from '@material-ui/styles';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 class PaletteList extends Component {
 	goToPalette(id) {
@@ -18,17 +19,19 @@ class PaletteList extends Component {
 						<h1 className={classes.heading}>Palette colors</h1>
 						<Link to='/palette/new'>Create Palette</Link>
 					</nav>
-					<div className={classes.palettes}>
+					<TransitionGroup className={classes.palettes}>
 						{palettes.map((palette) => (
-							<MiniPalette
-								key={palette.id}
-								{...palette}
-								handleClick={() => this.goToPalette(palette.id)}
-								handleDelete={deletePalette}
-								id={palette.id}
-							/>
+							<CSSTransition key={palette.id} className='fade' timeout={500}>
+								<MiniPalette
+									key={palette.id}
+									{...palette}
+									handleClick={() => this.goToPalette(palette.id)}
+									handleDelete={deletePalette}
+									id={palette.id}
+								/>
+							</CSSTransition>
 						))}
-					</div>
+					</TransitionGroup>
 				</div>
 			</div>
 		);
